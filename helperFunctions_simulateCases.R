5c66cf52926ae8103ef71532636c1e9587dbe47c
mendoza-cm <mendozachristina1729@gmail.com>
2025-05-06 12:24:58 -0600

    Update helperFunctions_simulateCases.R

--- helperFunctions_simulateCases.R
+++ helperFunctions_simulateCases.R
@@ -2,7 +2,7 @@ library(tidyverse)
 human_startTimes <- function(cases=10, do_replace = FALSE, min = 3000, max = 100000){
 	id <- sample(min:max, size = cases, replace = do_replace)
 	session_start_time <- sample(
-		as.numeric(as.POSIXlt("2024-04-30 01:02:21 UTC")):as.numeric(as.POSIXlt("2025-04-30 08:42:14 UTC")), 
+		as.numeric(as.POSIXct("2024-04-30 01:02:21 UTC")):as.numeric(as.POSIXct("2025-04-30 08:42:14 UTC")), 
 		size = cases)
 	human_startTimesDF <- data.frame(id = id,session_start_time = session_start_time) %>%
 					dplyr::group_by(id, session_start_time) %>%
@@ -10,31 +10,40 @@ human_startTimes <- function(cases=10, do_replace = FALSE, min = 3000, max = 100
 						ungroup(.) %>%
 					data.frame(.) %>%					
 					dplyr::mutate(type = "human",
-							  session_start_time = as.POSIXlt(session_start_time)) 
+							  session_start_time = as.POSIXct(session_start_time)) 
 		return(human_startTimesDF[,c("id","session_id","session_start_time","type")])
 
 }
 
-human_timesdistributionDF <- function(human_startTimesDF,timeLimit = 10800, maxClicks = 100000){
+##
+human_timesdistributionDF <- function(human_startTimesDF,timeLimit = 10800, meanClicks = NA){
 		randomTimeLimits <- sample(30:timeLimit, size = dim(human_startTimesDF)[1])
 
 		human_TimesDF <- data.frame(human_startTimesDF,randomTimeLimits) %>%
-						dplyr::mutate(session_end_time = ymd_hms(session_start_time) + randomTimeLimits)
+						dplyr::mutate(session_end_time = session_start_time + randomTimeLimits)
 
 		clicks_long <- data.frame() 
 
 		for(i in 1:nrow(human_TimesDF)){
-			if(human_TimesDF[i,"randomTimeLimits"] <= 30){
-				times_clicked <- sample(1:5, size = 1)
-			} else if(human_TimesDF[i,"randomTimeLimits"] <= 120){
-				times_clicked <- sample(10:30, size = 1)
-				} else if(human_TimesDF[i,"randomTimeLimits"] <= 1800){
-					times_clicked <- sample(40:200, size = 1)
-					} else
- 					  times_clicked <- sample(300:2000, size = 1)
+		   if(!is.na(meanClicks)){
+			times_clicked <- abs(rnorm(1,mean=meanClicks,sd=12.5))
+			} else if(human_TimesDF[i,"randomTimeLimits"] <= 30){
+				times_clicked <- abs(rnorm(1,mean=3,sd=0.5))
+				} else if(human_TimesDF[i,"randomTimeLimits"] <= 120){
+				times_clicked <- abs(rnorm(1,mean=15,sd=0.5))
+					} else if(human_TimesDF[i,"randomTimeLimits"] <= 1800){
+					times_clicked <- abs(rnorm(1,mean=50,sd=1))
+						} else
+ 					  times_clicked <- abs(rnorm(1,mean=200,sd=25))
 
-			timesToAdd <- sort(runif(times_clicked, 0, human_TimesDF[i, "randomTimeLimits"]))
-			click_times <- human_TimesDF[i, "session_start_time"] + timesToAdd
+			timesToAdd <- abs(rnorm(round(times_clicked), mean = 30,  sd = 2.5))
+			click_times <- human_TimesDF[i, "session_start_time"] + cumsum(timesToAdd)
+
+			click_times <- click_times[click_times <= human_TimesDF[i, "session_end_time"]]
+
+			#if (length(click_times) < 2) next
+                  times_clicked <- length(click_times)
+			
 			temp <- data.frame(
   					id = rep(human_TimesDF[i, "id"], times_clicked),
   					session_id = rep(human_TimesDF[i, "session_id"], times_clicked),
@@ -44,7 +53,7 @@ human_timesdistributionDF <- function(human_startTimesDF,timeLimit = 10800, maxC
 						dplyr::select(id,session_id,type,time = session_start_time)
 
 
-			clicks_long <- data.frame(rbind(clicks_long,start_row,temp))
+			clicks_long <- dplyr::bind_rows(clicks_long,start_row,temp)
 		}
 
 
@@ -56,6 +65,7 @@ human_timesdistributionDF <- function(human_startTimesDF,timeLimit = 10800, maxC
 
 
 ###
+###
 
 
 
@@ -63,7 +73,7 @@ human_timesdistributionDF <- function(human_startTimesDF,timeLimit = 10800, maxC
 bot_startTimes <- function(cases=10, do_replace = FALSE, min = 3000, max = 100000){
 	id <- sample(min:max, size = cases, replace = do_replace)
 	session_start_time <- sample(
-		as.numeric(as.POSIXlt("2024-04-30 01:02:21 UTC")):as.numeric(as.POSIXlt("2025-04-30 08:42:14 UTC")), 
+		as.numeric(as.POSIXct("2024-04-30 01:02:21 UTC")):as.numeric(as.POSIXct("2025-04-30 08:42:14 UTC")), 
 		size = cases)
 	bot_startTimesDF <- data.frame(id = id,session_start_time = session_start_time) %>%
 					dplyr::group_by(id, session_start_time) %>%
@@ -71,34 +81,41 @@ bot_startTimes <- function(cases=10, do_replace = FALSE, min = 3000, max = 10000
 						ungroup(.) %>%
 					data.frame(.) %>%					
 					dplyr::mutate(type = "bot",
-							  session_start_time = as.POSIXlt(session_start_time)) 
+							  session_start_time = as.POSIXct(session_start_time)) 
 		return(bot_startTimesDF[,c("id","session_id","session_start_time","type")])
 
 }
 
 
-bot_timesdistributionDF <- function(bot_startTimesDF,timeLimit = 10800, maxClicks = 100000){
+bot_timesdistributionDF <- function(bot_startTimesDF,timeLimit = 10800, meanClicks = NA){
 		randomTimeLimits <- sample(30:timeLimit, size = dim(bot_startTimesDF)[1])
 		bot_TimesDF <- data.frame(bot_startTimesDF,randomTimeLimits) %>%
-						dplyr::mutate(session_end_time = ymd_hms(session_start_time) + randomTimeLimits)
+						dplyr::mutate(session_end_time = session_start_time + randomTimeLimits)
 
 		clicks_long <- data.frame() 
 
 		for(i in 1:nrow(bot_TimesDF)){
-			if(bot_TimesDF[i,"randomTimeLimits"] <= 30){
-				times_clicked <- sample(1:10, size = 1)
+		  if(!is.na(meanClicks)){
+			times_clicked <- abs(rnorm(1,mean=meanClicks,sd=12.5))
+
+		   } else if(bot_TimesDF[i,"randomTimeLimits"] <= 30){
+				times_clicked <- abs(rnorm(1,mean=25,sd=1.5))
 			} else if(bot_TimesDF[i,"randomTimeLimits"] <= 120){
-				times_clicked <- sample(100:500, size = 1)
+				times_clicked <- abs(rnorm(1,mean=250,sd=10.5))
 				} else if(bot_TimesDF[i,"randomTimeLimits"] <= 1800){
-					times_clicked <- sample(1000:10000, size = 1)
+					times_clicked <- abs(rnorm(1,mean=500,sd=1.5))
 					} else
-					  times_clicked <- sample(20000:100000, size = 1)
+					  times_clicked <- abs(rnorm(1,mean=1000,sd=100.5))
 			
 
+							
+			timesToAdd <- rexp(round(times_clicked), 1/10)
+			click_times <- bot_TimesDF[i, "session_start_time"] + cumsum(timesToAdd)
 
-			timesToAdd <- seq(0, bot_TimesDF[i,"randomTimeLimits"], length.out = times_clicked)	
-			click_times <- bot_TimesDF[i, "session_start_time"] + timesToAdd + rnorm(times_clicked, mean = 0, sd = 0.05)
-			# click_times <- seq(0, randomTimeLimits, length.out = times_clicked)	
+			click_times <- click_times[click_times <= bot_TimesDF[i, "session_end_time"]]
+			if (length(click_times) < 2) next
+                  times_clicked <- length(click_times)
+
 			temp <- data.frame(
   					id = rep(bot_TimesDF[i, "id"], times_clicked),
   					session_id = rep(bot_TimesDF[i, "session_id"], times_clicked),
@@ -108,7 +125,7 @@ bot_timesdistributionDF <- function(bot_startTimesDF,timeLimit = 10800, maxClick
 						dplyr::select(id,session_id,type,time = session_start_time)
 
 
-			clicks_long <- data.frame(rbind(clicks_long,start_row,temp))
+			clicks_long <- dplyr::bind_rows(clicks_long,start_row,temp)
 		}
 
 
