dcd3233930fe37b1ed9eba067a983cc201010eed
Christina Mendoza <mendozachristina1729@gmail.com>
2025-05-16 17:56:29 -0600

    updated python folder

--- python/read_simulatedData_logReg_andPlots.py
+++ python/read_simulatedData_logReg_andPlots.py
@@ -5,7 +5,7 @@ import os
 # from matplotlib import pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-
+import pandas as pd
 
 #from sklearn.metrics import accuracy_score, classification_report
 #from sklearn.datasets import load_breast_cancer
@@ -20,7 +20,7 @@ test_dat = converted["test_data"]
 
 colNames = training_dat.columns
 
-myTrainVars = training_dat[['rate','sd_inter_click','isBot']]
+myTrainVars = training_dat[['rate','sd_inter_click','isBot','type']]
 # Plot settings
 fig, ax = plt.subplots(figsize=(4,3))
 x_min, x_max, y_min, y_max = -3,14,0,8
@@ -32,10 +32,33 @@ ax.set_title("Samples in two-dimensional feature space")
 plt.xlabel("SD", fontsize=16)
 plt.ylabel("Rate", fontsize = 16)
 plt.title("Scatter Plot of Rate vs SD")
-#_ = plt.show()
+plt.show()
 
 plt.savefig("visualizations/scatterPlot_sdRate.png")
 plt.close()
+
+
+fig, ax = plt.subplots(figsize=(4,3))
+x_min, x_max, y_min, y_max = -3,14,0,8
+ax.set(xlim=(x_min,x_max), ylim = (-1,y_max))
+for label, group in myTrainVars.groupby("type"):
+      ax.scatter(
+            group["sd_inter_click"],
+            group["rate"],
+            s = 150,
+            label = label,
+            edgecolors = "k"
+      )
+ax.legend(loc="upper right", title = "Classes")
+ax.set_title("Samples in two-dimensional feature space")
+plt.xlabel("SD", fontsize=16)
+plt.ylabel("Rate", fontsize = 16)
+plt.title("Scatter Plot of Rate vs SD")
+#plt.show()
+
+plt.savefig("visualizations/scatterPlot_sdRate_yLabels.png")
+plt.close()
+
 
 # prep for logistic regression; L2 is ridge regression
 X = myTrainVars[['rate']]
@@ -148,3 +171,83 @@ plt.scatter(test_dat["sd_inter_click"],test_dat["isBot"].astype(int), c = "red")
 
 plt.title("Sigmoid Curve at Rate ~ {}".format(round(x1Rate[52,0],2)))
 plt.show()
+
+## predict test data cases - calculate accuracy
+myTestVars = test_dat[['rate','sd_inter_click']]
+
+
+test_dat['predicted_probability'] = logReg2_R.predict_proba(myTestVars)[:,1]
+test_dat['logit_score'] = logReg2_R.decision_function(myTestVars)
+
+#myCuts = np.linspace(0,1,11) 
+#for prob in myCut
+
+def predictive_values_function(df, cuts, class_col, score_col):
+      myPredictiveValues = pd.DataFrame()
+
+      for cut in cuts:
+            score_above_ind = df[score_col] > cut
+            score_notAbove_ind = df[score_col] <= cut
+            
+
+
+            isBot_ind = (df[class_col]).astype(int) == 1
+            isHuman_ind = (df[class_col]).astype(int) == 0
+
+            above_cut_df = df[score_above_ind] 
+            below_cut_df = df[score_notAbove_ind] 
+
+            tp = (score_above_ind & isBot_ind).sum()
+            tn = (score_notAbove_ind & isHuman_ind).sum()
+
+            ppv = round(tp/len(above_cut_df),2) if len(above_cut_df) > 0 else np.nan
+            npv = round(tn/len(below_cut_df),2) if len(below_cut_df) > 0 else np.nan
+            SR = round(len(above_cut_df)/df.shape[0],2)
+
+            temp = pd.DataFrame([{
+                  "cut_point": cut,
+                  "PPV": ppv,
+                  "NPV": npv,
+                  "selection_ratio": SR
+            }])
+            myPredictiveValues = pd.concat([myPredictiveValues,temp], ignore_index=True)
+
+      return myPredictiveValues
+      
+myCuts = np.linspace(0,1,11) 
+result = predictive_values_function(df = test_dat, cuts = myCuts, class_col = 'isBot', score_col = 'predicted_probability')
+
+predictive_values_function(test_dat, myCuts, 'isBot', 'predicted_probability')
+
+
+
+def ROC_values_function(df,cuts,class_col,scores_col):
+      myROC_values = pd.DataFrame()
+      isBot_ind = (df[class_col]).astype(int) == 1
+      isHuman_ind = (df[class_col]).astype(int) == 0
+
+      for cut in cuts:
+            score_above_ind = df[scores_col] > cut
+            score_notAbove_ind = df[scores_col] <= cut
+
+            true_positives = (isBot_ind & score_above_ind).sum()
+            true_negatives = (isHuman_ind & score_notAbove_ind).sum()
+
+            true_pos_rate = round(true_positives/isBot_ind.sum(),2)
+            true_neg_rate = round(true_negatives/isHuman_ind.sum(),2)
+
+            false_positive_rate = 1 - true_neg_rate
+            accuracy = round((true_positives + true_negatives)/df.shape[0],2)
+
+            temp = pd.DataFrame([{
+                  "cutpoint": cut,
+                  "TPR": true_pos_rate,
+                  "FPR": false_positive_rate,
+                  "accuracy": f"{accuracy*100:.2f}%"
+            }])
+            
+            myROC_values = pd.concat([myROC_values,temp], ignore_index = True)
+
+      return myROC_values
+
+ROC_values_function(df = test_dat,cuts = myCuts,class_col = 'isBot',scores_col = 'predicted_probability')
