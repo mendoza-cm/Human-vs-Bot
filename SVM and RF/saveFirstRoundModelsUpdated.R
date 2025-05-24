577d451a0f7e2fb5f4f7b65a74c19d30730ae85c
Christina Mendoza <mendozachristina1729@gmail.com>
2025-05-23 23:53:47 -0600

    updated ML models - first round and scripts

---
+++ SVM and RF/saveFirstRoundModelsUpdated.R
@@ -0,0 +1,71 @@
+rm(list=ls())
+library(e1071)
+library(tidyverse)
+library(randomForest)
+# datAll <- read.csv("~/myGit_R/Data/bot_vs_human/data_all.csv")
+
+## this file assumes data that will be used for training the models is called training data
+
+## change directory for downloaded data
+load("./data/human_vs_dataUpdated.RData")
+
+myTraining_dat <- dplyr::mutate_at(training_data, 
+			.vars = vars(isBot), ~ as.factor(.)) %>%
+		dplyr::select(rate,
+				  sd_inter_response,
+				  isBot)
+
+ 
+
+
+##### train models
+# linear SVM
+model_linear <- svm(isBot ~ rate + sd_inter_response, 
+		data = myTraining_dat, 
+		kernel = "linear",
+		probability = TRUE,
+		type = "C-classification")
+
+# radial kernel (RBF)
+model_rbf <- svm(isBot ~ rate + sd_inter_response, 
+		data = myTraining_dat, 
+		kernel = "radial",
+		probability = TRUE,
+		type = "C-classification")
+
+# polynomial kernel
+model_poly2 <- svm(isBot ~ rate + sd_inter_response, 
+		data = myTraining_dat, 
+		kernel = "polynomial", 
+		degree = 2,
+		probability = TRUE,
+		type = "C-classification")
+
+model_poly3 <- svm(isBot ~ rate + sd_inter_response, 
+		data = myTraining_dat, 
+		kernel = "polynomial", 
+		degree = 3,
+		probability = TRUE,
+		type = "C-classification")
+
+
+myModels <- list()
+myModels$svm <- list(
+	linear = model_linear,
+	rfb = model_rbf,
+	poly_degree2 = model_poly2,
+	poly_degree3 = model_poly2
+	)
+
+
+## random forest does not actually need a training and testing sample since it uses OOB; 
+### for comparison purposes with our data, we utilize the training and test data as we did with the SVM
+rf_model = randomForest(factor(isBot) ~ ., 
+		data = training_data[,c("rate","sd_inter_response","duration","mean_inter_response","responses_per_session","isBot")], 
+		type = c("class","prob"))
+
+myModels$RF <- rf_model
+
+#save(myModels, file = "./SVM and RF/svmRF_modelsUpdated.RData")
+#### all models use default scale = TRUE
+
